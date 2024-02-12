0b0674cc8b09a77fa073694cdfc43e4a5705d53d
VaibhavDhamanage <vaibhavdhamanage>
2024-02-12 16:53:37 +0530

    New Project ReactJs with Redux and styling With TailwindCss

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import SignUp from "./Component/SignUp";
+import Navbar from "./Component/Header/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <SignUp />
+    </>
+  );
+}
+
+export default App;
