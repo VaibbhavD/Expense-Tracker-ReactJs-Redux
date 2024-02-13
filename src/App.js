1531a055aabed76a1c1338767cb8a679416d00c0
VaibhavDhamanage <vaibhavdhamanage>
2024-02-13 17:55:49 +0530

    create Route path using Routes and Route

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./App.css";
-// import SignUp from "./Component/SignUp";
-import Navbar from "./Component/Header/Navbar";
-import Login from "./Component/Sign$Login.js/Login";
+import SignUp from "./Component/Pages/SignUp";
+import Login from "./Component/Pages/Login";
+import Layout from "./Component/Pages/Layout";
+import { Route, Router, Routes } from "react-router-dom";
+import Profile from "./Component/Pages/Profile";
+import AuthContext from "./Store/Auth-Context";
 
 function App() {
+  const context = useContext(AuthContext);
+
   return (
-    <>
-      <Navbar />
-      <Login />
-    </>
+    <Layout>
+      <main>
+        <Routes>
+          {context.IsLoggedIn && (
+            <Route path="/profile" element={<Profile />} />
+          )}
+          {!context.IsLoggedIn && (
+            <Route path="/login" element={<Login />} exact />
+          )}
+          {!context.IsLoggedIn && <Route path="/signup" element={<SignUp />} />}
+          {!context.IsLoggedIn && <Route path="*" element={<Login />} />}
+        </Routes>
+      </main>
+    </Layout>
   );
 }
 
