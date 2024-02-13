71c23861e8b6ea85712441237289356813262c9b
VaibhavDhamanage <vaibhavdhamanage>
2024-02-13 17:56:03 +0530

    Add logout button

--- src/Component/Header/Navbar.js
+++ src/Component/Header/Navbar.js
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import AuthContext from "../../Store/Auth-Context";
 
 const Navbar = () => {
+  const context = useContext(AuthContext);
+
   return (
     <div class="flex bg-gray-800 text-white top-0 py-3 flex-wrap justify-around bg-silver">
       <h1 class="text-lg font-semibold">Todo app</h1>
@@ -9,6 +12,9 @@ const Navbar = () => {
         <li>product</li>
         <li>about</li>
         <li>contact</li>
+        <li>
+          <button onClick={() => context.Logout()}> Logout </button>
+        </li>
       </ul>
     </div>
   );
