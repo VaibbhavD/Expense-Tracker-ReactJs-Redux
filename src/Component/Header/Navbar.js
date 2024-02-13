8a6f610bead9f25c6532ab25dd900464352fbaae
VaibhavDhamanage <vaibhavdhamanage>
2024-02-13 18:22:58 +0530

    Added Link for Navigate Routes

--- src/Component/Header/Navbar.js
+++ src/Component/Header/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useContext } from "react";
 import AuthContext from "../../Store/Auth-Context";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const context = useContext(AuthContext);
@@ -7,14 +8,15 @@ const Navbar = () => {
   return (
     <div class="flex bg-gray-800 text-white top-0 py-3 flex-wrap justify-around bg-silver">
       <h1 class="text-lg font-semibold">Todo app</h1>
-      <ul class="flex gap-[40px] text-m">
-        <li>Home</li>
-        <li>product</li>
-        <li>about</li>
-        <li>contact</li>
-        <li>
-          <button onClick={() => context.Logout()}> Logout </button>
-        </li>
+      <ul class="flex gap-[40px] text-m cursor-pointer">
+        {!context.IsLoggedIn && <Link to={"/login"}>login</Link>}
+        {!context.IsLoggedIn && <Link to={"/signup"}>SignUp</Link>}
+        {context.IsLoggedIn && <Link to={"/profile"}>Profile</Link>}
+        {context.IsLoggedIn && (
+          <Link to={"/login"}>
+            <button onClick={() => context.Logout()}> Logout </button>
+          </Link>
+        )}
       </ul>
     </div>
   );
