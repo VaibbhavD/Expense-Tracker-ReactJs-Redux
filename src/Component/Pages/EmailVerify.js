6d43e60c532296467a81c07ee576989350b3adf8
VaibhavDhamanage <vaibhavdhamanage>
2024-02-14 11:19:43 +0530

    style changes

--- src/Component/Pages/EmailVerify.js
+++ src/Component/Pages/EmailVerify.js
@@ -29,7 +29,7 @@ const VerifyEmail = () => {
   }
 
   return (
-    <div className="w-full h-full flex justify-center">
+    <div className=" absolute mt-10 top-96 flex justify-center">
       <button
         type="button"
         onClick={VerifyHandler}
