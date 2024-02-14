58d5f7bab4827fdb7149494d5719b3393140257e
VaibhavDhamanage <vaibhavdhamanage>
2024-02-14 15:38:22 +0530

    Create ExpansePge return Form and Show items component

---
+++ src/Component/Expense/ExpensePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ShowExpense from "./ShowExpense";
+import ExpenseForm from "./ExpenseForm";
+
+const ExpensePage = () => {
+  return (
+    <>
+      <ExpenseForm />
+      <ShowExpense />
+    </>
+  );
+};
+export default ExpensePage;
