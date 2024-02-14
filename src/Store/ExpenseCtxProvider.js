7212a492d5cd3e67c4e55118fdd9630028b1bea9
VaibhavDhamanage <vaibhavdhamanage>
2024-02-14 16:33:40 +0530

    Expesex data store in firebase database using Rest APi

--- src/Store/ExpenseCtxProvider.js
+++ src/Store/ExpenseCtxProvider.js
@@ -1,13 +1,51 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ExpenseContext from "./ExpenseContext";
 
 const ExpenseCtxProvider = (props) => {
   const [Expenses, setExpenses] = useState([]);
+  const [ex, setex] = useState({});
 
   const AddExpense = (item) => {
-    console.log(item);
     setExpenses((prev) => [...prev, item]);
+    fetch(
+      "https://authentication-1db8c-default-rtdb.firebaseio.com/expens.json",
+      {
+        method: "POST",
+        body: JSON.stringify(item),
+      }
+    )
+      .then((res) => {
+        if (res.ok) {
+        } else {
+          res.json().then((data) => alert(data.error.message));
+        }
+      })
+      .catch((error) => alert(error.message));
   };
+
+  useEffect(() => {
+    fetch(
+      "https://authentication-1db8c-default-rtdb.firebaseio.com/expens.json"
+    )
+      .then((res) => {
+        if (res.ok) {
+          res.json().then((data) => {
+            if (data) {
+              setExpenses(Object.values(data));
+            }
+          });
+        } else {
+          res.json().then((data) => {
+            for (const key in data) {
+              setex(Object.entries(data[key]));
+            }
+          });
+        }
+      })
+      .catch((error) => console(error.message));
+  }, []);
+
+  console.log(ex);
 
   const RemoveExpense = (item) => {
     Expenses.filter((it) => it.id !== item.id);
