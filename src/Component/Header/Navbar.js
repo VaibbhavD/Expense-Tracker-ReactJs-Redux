4fea3e8a4f72d3a67502c5cad20f8bd0abb41ae7
VaibhavDhamanage <vaibhavdhamanage>
2024-02-18 17:04:37 +0530

    Add dashboard tab

--- src/Component/Header/Navbar.js
+++ src/Component/Header/Navbar.js
@@ -3,6 +3,7 @@ import AuthContext from "../../Store/ContextApi/Auth-Context";
 import { Link, useNavigate } from "react-router-dom";
 import { AuthAction } from "../../Store/Redux/AuthSlice";
 import { useDispatch, useSelector } from "react-redux";
+import ExpensePage from "../Expense/ExpensePage";
 
 const Navbar = () => {
   const context = useContext(AuthContext);
@@ -19,12 +20,13 @@ const Navbar = () => {
   return (
     <div class="flex bg-gray-800 text-white top-0 py-3 flex-wrap justify-around bg-silver">
       <h1 class="text-lg font-semibold">
-        <Link to={"/"}>Todo app</Link>
+        <Link to={"/"}>Expense TracKeR</Link>
       </h1>
       <ul class="flex gap-[40px] text-m cursor-pointer">
         {!IsLoggedIn && <Link to={"/login"}>login</Link>}
         {!IsLoggedIn && <Link to={"/signup"}>SignUp</Link>}
         {IsLoggedIn && <Link to={"/profile"}>Profile</Link>}
+        {IsLoggedIn && <Link to={"/"}>Dashboard</Link>}
         {IsLoggedIn && <button onClick={LogoutHadler}> Logout </button>}
       </ul>
     </div>
