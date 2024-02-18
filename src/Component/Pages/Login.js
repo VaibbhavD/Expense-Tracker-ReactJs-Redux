42cb555e4cb5f57f2c4e154904596a2b8e7ba561
VaibhavDhamanage <vaibhavdhamanage>
2024-02-18 19:09:01 +0530

    dark Mode classes Added

--- src/Component/Pages/Login.js
+++ src/Component/Pages/Login.js
@@ -8,6 +8,7 @@ const Login = () => {
   const EmailRef = useRef();
   const PasswordRef = useRef();
   const Islogin = useSelector((state) => state.Auth.IsLoggedIn);
+  const darkmode = useSelector((state) => state.Auth.DarkMode);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -57,57 +58,56 @@ const Login = () => {
 
   return (
     <>
-      {Islogin && <Profile />}
-      <div class="bg-grey-lighter min-h-full flex flex-col mt-20">
-        {!Islogin && (
-          <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-            <form
-              onSubmit={Submit}
-              class="bg-white px-6 py-8 rounded shadow-md text-black w-full"
+      <div class={` h-screen  ${darkmode ? "bg-black" : "bg-stone-200"}`}>
+        <div class=" container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
+          <form
+            onSubmit={Submit}
+            class={`${
+              darkmode ? "bg-stone-200" : "bg-white"
+            } px-6 py-8 rounded shadow-md text-black w-full mt-20`}
+          >
+            <h1 class="mb-8 text-3xl text-center font-bold">Login</h1>
+
+            <input
+              type="text"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="email"
+              ref={EmailRef}
+              placeholder="Email"
+              required
+            />
+
+            <input
+              type="password"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="password"
+              ref={PasswordRef}
+              placeholder="Password"
+              min={7}
+              required
+            />
+
+            <button
+              type="submit"
+              class="w-full text-center py-3 rounded  bg-blue-600 hover:bg-blue-500 text-stone-50 font-bold hover:bg-green-dark focus:outline-none my-1"
             >
-              <h1 class="mb-8 text-3xl text-center">Login</h1>
-
-              <input
-                type="text"
-                class="block border border-grey-light w-full p-3 rounded mb-4"
-                name="email"
-                ref={EmailRef}
-                placeholder="Email"
-                required
-              />
-
-              <input
-                type="password"
-                class="block border border-grey-light w-full p-3 rounded mb-4"
-                name="password"
-                ref={PasswordRef}
-                placeholder="Password"
-                min={7}
-                required
-              />
-
-              <button
-                type="submit"
-                class="w-full text-center py-3 rounded  bg-blue-600 hover:bg-blue-500 text-stone-50 font-bold hover:bg-green-dark focus:outline-none my-1"
-              >
-                Login
-              </button>
-              <Link to={"/forget"} class="text-grey-dark mt-6 flex justify-end">
-                Forget Password?
-              </Link>
-            </form>
-            <div class="text-grey-dark mt-6">
-              Don't have a Account?
-              <Link
-                class="no-underline border-b border-blue text-blue"
-                to={"/signup"}
-              >
-                Sign Up
-              </Link>
-              .
-            </div>
+              Login
+            </button>
+            <Link to={"/forget"} class="text-grey-dark mt-6 flex justify-end">
+              Forget Password?
+            </Link>
+          </form>
+          <div class={`text-grey-dark mt-6 ${darkmode ? "text-stone-50" : ""}`}>
+            Don't have a Account?
+            <Link
+              class="no-underline border-b border-blue text-blue"
+              to={"/signup"}
+            >
+              Sign Up
+            </Link>
+            .
           </div>
-        )}
+        </div>
       </div>
     </>
   );
--- src/Component/Pages/SignUp.js
+++ src/Component/Pages/SignUp.js
@@ -2,6 +2,7 @@ import React, { useContext, useRef } from "react";
 import AuthContext from "../../Store/ContextApi/Auth-Context";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const SignUp = () => {
   const EmailRef = useRef();
@@ -9,6 +10,7 @@ const SignUp = () => {
   const RePasswordRef = useRef();
   const context = useContext(AuthContext);
   const navigate = useNavigate();
+  const darkmode = useSelector((state) => state.Auth.DarkMode);
 
   const Submit = async (e) => {
     e.preventDefault();
@@ -58,58 +60,64 @@ const SignUp = () => {
   };
 
   return (
-    <div class="bg-grey-lighter min-h-screen flex flex-col">
-      <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2">
-        <form
-          onSubmit={Submit}
-          class="bg-white px-6 py-8 rounded shadow-md text-black w-full"
-        >
-          <h1 class="mb-8 text-3xl text-center">Sign up</h1>
-
-          <input
-            type="text"
-            class="block border border-grey-light w-full p-3 rounded mb-4"
-            name="email"
-            ref={EmailRef}
-            placeholder="Email"
-            required
-          />
-
-          <input
-            type="password"
-            class="block border border-grey-light w-full p-3 rounded mb-4"
-            name="password"
-            ref={PasswordRef}
-            placeholder="Password"
-            min={7}
-            required
-          />
-          <input
-            type="password"
-            class="block border border-grey-light w-full p-3 rounded mb-4"
-            name="confirm_password"
-            ref={RePasswordRef}
-            placeholder="Confirm Password"
-            min={7}
-            required
-          />
-
-          <button
-            type="submit"
-            class="w-full text-center py-3 rounded  bg-blue-600 hover:bg-blue-500 text-stone-50 font-bold hover:bg-green-dark focus:outline-none my-1"
+    <div
+      className={`w-screen min-h-svh ${darkmode ? "bg-black" : "bg-stone-200"}`}
+    >
+      <div class="bg-grey-lighter flex flex-col ">
+        <div class="container max-w-sm mx-auto flex-1 flex flex-col items-center justify-center px-2 mt-20">
+          <form
+            onSubmit={Submit}
+            class={`bg-white px-6 py-8 rounded shadow-md text-black w-full ${
+              darkmode ? "bg-stone-100" : ""
+            }`}
           >
-            Create Account
-          </button>
-        </form>
-        <div class="text-grey-dark mt-6">
-          Already have an account?
-          <Link
-            class="no-underline border-b border-blue text-blue"
-            to={"/login"}
-          >
-            Log in
-          </Link>
-          .
+            <h1 class="mb-8 text-3xl text-center font-bold">Sign up</h1>
+
+            <input
+              type="text"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="email"
+              ref={EmailRef}
+              placeholder="Email"
+              required
+            />
+
+            <input
+              type="password"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="password"
+              ref={PasswordRef}
+              placeholder="Password"
+              min={7}
+              required
+            />
+            <input
+              type="password"
+              class="block border border-grey-light w-full p-3 rounded mb-4"
+              name="confirm_password"
+              ref={RePasswordRef}
+              placeholder="Confirm Password"
+              min={7}
+              required
+            />
+
+            <button
+              type="submit"
+              class="w-full text-center py-3 rounded  bg-blue-600 hover:bg-blue-500 text-stone-50 font-bold hover:bg-green-dark focus:outline-none my-1"
+            >
+              Create Account
+            </button>
+          </form>
+          <div class={`text-grey-dark mt-6 ${darkmode ? "text-stone-50" : ""}`}>
+            Already have an account?
+            <Link
+              class="no-underline border-b border-blue text-blue"
+              to={"/login"}
+            >
+              Log in
+            </Link>
+            .
+          </div>
         </div>
       </div>
     </div>
