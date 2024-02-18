cca7a89f38bf34ca018bb4b55ae728fc6383f30f
VaibhavDhamanage <vaibhavdhamanage>
2024-02-18 19:07:48 +0530

    butoon classes conditionally

--- src/Component/Expense/ExpenseTotal.js
+++ src/Component/Expense/ExpenseTotal.js
@@ -9,27 +9,18 @@ const ExpenseTotal = () => {
   return (
     <p className="w-1/2 h-12 text-right text-lg font-bold flex justify-center space-x-2 px-4 bg-gray-100">
       <p className="flex justify-center m-auto">
-        {total > 10000 ? (
-          <>
-            <button
-              type="button"
-              className="text-center"
-              class="inline-flex justify-center py-1 px-4 border border-transparent shadow-sm text-lm font-medium rounded-md text-white bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2"
-            >
-              Premium
-            </button>
-          </>
-        ) : (
-          <>
-            <button
-              type="button"
-              className="text-center"
-              class="inline-flex justify-center py-1 px-4 border border-transparent shadow-sm text-lm font-medium rounded-md text-white bg-indigo-300 focus:outline-none focus:ring-2 focus:ring-offset-2"
-            >
-              Premium
-            </button>
-          </>
-        )}
+        <button
+          type="button"
+          className="text-center"
+          class={`${
+            total > 10000
+              ? "inline-flex justify-center py-1 px-4 border border-transparent shadow-sm text-lm font-medium rounded-md text-white bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2"
+              : "inline-flex justify-center py-1 px-4 border border-transparent shadow-sm text-lm font-medium rounded-md text-white bg-indigo-300 focus:outline-none focus:ring-2 focus:ring-offset-2"
+          }`}
+        >
+          Premium
+        </button>
+
         <p className="ml-2 text-sm m-auto">
           (Access While Expenses Over Rs.10000)
         </p>
