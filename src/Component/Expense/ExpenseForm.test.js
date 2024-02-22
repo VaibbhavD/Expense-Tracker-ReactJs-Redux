4ccc22f71219fb2f33f821becc4b257d30750767
VaibhavDhamanage <vaibhavdhamanage>
2024-02-22 10:41:24 +0530

    Add test.js

---
+++ src/Component/Expense/ExpenseForm.test.js
@@ -0,0 +1,60 @@
+import ExpenseForm from "./ExpenseForm";
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import store from "../../Store/Redux/ReduxStore";
+
+describe("Async Component", () => {
+  test("renders", async () => {
+    render(
+      <Provider store={store}>
+        <ExpenseForm />
+      </Provider>
+    );
+
+    const items = await screen.findByText("Description");
+    expect(items).toBeInTheDocument();
+  });
+
+  test("renders", async () => {
+    render(
+      <Provider store={store}>
+        <ExpenseForm />
+      </Provider>
+    );
+
+    const items = await screen.findAllByRole("button");
+    expect(items).not.toHaveLength(0);
+  });
+
+  test("renders", async () => {
+    render(
+      <Provider store={store}>
+        <ExpenseForm />
+      </Provider>
+    );
+
+    const items = await screen.findAllByRole("alert");
+    expect(items).toBeChecked();
+  });
+
+  test("renders", async () => {
+    render(
+      <Provider store={store}>
+        <ExpenseForm />
+      </Provider>
+    );
+
+    const items = await screen.findByText("Amount");
+    expect(items).toBeInTheDocument();
+  });
+  test("renders", async () => {
+    render(
+      <Provider store={store}>
+        <ExpenseForm />
+      </Provider>
+    );
+
+    const items = await screen.findByText("Categeory");
+    expect(items).toBeInTheDocument();
+  });
+});
